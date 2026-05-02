eaed822411d435a12e53c3068facdb1819f0581c
NavoVerse <navonotfound@gmail.com>
2026-05-02 19:38:57 +0530

    clean up

--- Backend/scratch/test_sqlite.js
+++
@@ -1,4 +0,0 @@
-const Database = require('better-sqlite3');
-const db = new Database(':memory:');
-console.log('better-sqlite3 works!');
-db.close();
--- Backend/scratch/test_sqlite3.js
+++
@@ -1,6 +0,0 @@
-const sqlite3 = require('sqlite3').verbose();
-const db = new sqlite3.Database(':memory:');
-db.serialize(() => {
-    console.log('sqlite3 works!');
-});
-db.close();
